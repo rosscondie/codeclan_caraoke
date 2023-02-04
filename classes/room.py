2c1686506827d4006fc918fcf66824f621fcc63d
Ross Condie <rosscondie@gmail.com>
2023-02-04 13:51:24 +0100

    added song to room

--- classes/room.py
+++ classes/room.py
@@ -2,3 +2,7 @@ class Room:
     def __init__(self, name):
         self.name = name
         self.songs = []
+        self.capacity = []
+
+    def add_song(self, new_song):
+        self.songs.append(new_song)
--- tests/room_test.py
+++ tests/room_test.py
@@ -6,16 +6,11 @@ from classes.song import Song
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room_1 = Room("The Glitter Room")
-        self.room_2 = Room("The Basement Room")
-        self.room_3 = Room("The Rock Room")
+        self.room_2 = Room("The Rock Room")
         self.guest_1 = Guest("Jimi Hendrix")
         self.guest_2 = Guest("Bruce Springsteen")
-        self.guest_3 = Guest("Frank Sinatra")
-        self.song = Song("Welcome to the Jungle", "Guns N' Roses")
-        self.song = Song("Rocket Man", "Elton John")
-        self.song = Song("Ice Ice Baby", "Vanilla Ice")
-
-
+        self.song_1 = Song("Welcome to the Jungle", "Guns N' Roses")
+        self.song_2 = Song("Rocket Man", "Elton John")
 
     def test_room_has_name(self):
         expected = "The Glitter Room"
@@ -26,3 +21,27 @@ class TestRoom(unittest.TestCase):
         expected = []
         actual = self.room_1.songs
         self.assertEqual(expected, actual)
+
+    def test_can_add_songs(self):
+        song = Song("Paint it Black", "The Rolling Stones")
+        self.room_1.add_song(song)
+        expected = song
+        actual = self.room_1.songs[0]
+        self.assertEquals(expected, actual)
+
+    def test_can_add_song_to_rock_room(self):
+        song = Song("Sabbra Cadabra", "Metallica")
+        self.room_2.add_song(song)
+        expected = song
+        actual = self.room_2.songs[0]
+        self.assertEqual(expected, actual)
+
+
+    def test_room_1_has_capacity(self):
+        expected = []
+        actual = self.room_1.capacity
+        self.assertEqual(expected, actual)
+
+    
+
+    
