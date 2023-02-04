778e817b7cea8f81dbb609823fa8c23f42e2fc8b
Ross Condie <rosscondie@gmail.com>
2023-02-04 12:31:58 +0100

    deleted py_cache files

--- classes/song.py
+++ classes/song.py
@@ -3,5 +3,5 @@ class Song:
         self.title = title
         self.artist = artist
 
-    def title_by_artist():
-        pass
+    def title_by_artist(self):
+        return f"{self.title} by {self.artist}"
