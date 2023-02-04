d5eec1dfd73f9dde7c467c9648c2ed9720835b8a
Ross Condie <rosscondie@gmail.com>
2023-02-04 13:02:49 +0100

    added guests and songs

--- tests/room_test.py
+++ tests/room_test.py
@@ -5,14 +5,24 @@ from classes.song import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.room = Room("Glitter Room")
+        self.room_1 = Room("The Glitter Room")
+        self.room_2 = Room("The Basement Room")
+        self.room_3 = Room("The Rock Room")
+        self.guest_1 = Guest("Jimi Hendrix")
+        self.guest_2 = Guest("Bruce Springsteen")
+        self.guest_3 = Guest("Frank Sinatra")
+        self.song = Song("Welcome to the Jungle", "Guns N' Roses")
+        self.song = Song("Rocket Man", "Elton John")
+        self.song = Song("Ice Ice Baby", "Vanilla Ice")
+
+
 
     def test_room_has_name(self):
-        expected = "Glitter Room"
-        actual = self.room.name
+        expected = "The Glitter Room"
+        actual = self.room_1.name
         self.assertEqual(expected, actual)
 
     def test_room_has_songs(self):
         expected = []
-        actual = self.room.songs
+        actual = self.room_1.songs
         self.assertEqual(expected, actual)
